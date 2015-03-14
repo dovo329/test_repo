2b32de42a64648f08952276035ac02dc054403ec
Doug Voss <douglas.c.voss@gmail.com>
2015-03-13 21:18:18 -0600

    Initial Commit

---
+++ pre-course-git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pre-course-git
+//
+//  Created by Douglas Voss on 3/13/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
